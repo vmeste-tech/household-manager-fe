f952e70e82b90516812d6b6d60da100a2a241456
kolpakovee <eekolpakov@edu.hse.ru>
2024-11-24 21:46:09 +0300

    add docker file and update actions scrypt

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Позволяет слушать все сетевые интерфейсы (0.0.0.0)
+    port: 4173, // Устанавливает порт для разработки или preview
+  }
 })
