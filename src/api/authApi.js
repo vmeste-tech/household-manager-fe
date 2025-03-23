64b461d8c160ae7ecc9c14afd9a8cd0e9cb51e77
kolpakovee <eekolpakov@edu.hse.ru>
2025-03-23 10:57:27 +0300

    Revert "Update url: replace to user-service"
    This reverts commit 079aa6fd22edd570372565e0e9b97e12e1f7be17.

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,13 +1,10 @@
 import api from "./api";
 
 export const login = async (username, password) => {
-  const response = await api.post(
-    "http://user-service:8082/api/v1/auth/login",
-    {
-      username,
-      password,
-    }
-  );
+  const response = await api.post("http://localhost:8082/api/v1/auth/login", {
+    username,
+    password,
+  });
   const { access_token, refresh_token, expires_in, token_type } = response.data;
   // Сохраняем полученные токены в localStorage
   localStorage.setItem("access_token", access_token);
