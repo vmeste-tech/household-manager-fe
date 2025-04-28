df3349a2f99bc1cced5ab97f79b0d3667ecdeca4
kolpakovee <eekolpakov@edu.hse.ru>
2025-04-28 07:52:15 +0300

    Refactor API configuration to use environment variable for base URL and simplify service base URLs

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -4,13 +4,13 @@
  */
 
 // Получаем базовый URL из переменной окружения или используем значение по умолчанию
-const API_BASE_URL = "http://localhost";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost";
 
 // Конфигурация для каждого отдельного сервиса
 export const apiConfig = {
   // Сервис пользователей
   main: {
-    baseUrl: `${API_BASE_URL}:8082`,
+    baseUrl: `${API_BASE_URL}`,
   },
   // Сервис задач
   task: {
